5be317dc4196abf48a0b5217c02b59c5bb6bbdaf
Otávio Augusto <82326050+augustkkkj@users.noreply.github.com>
2024-10-12 20:43:10 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+entrar = 18
+
+print("seu nome é:", nome)
+print("e vc possui:", idade)
+
+if idade < 18:
+    print("vc não pode entrar");
+    
+    
+elif idade >= 18:
+      print("vc pode entrar")
+
